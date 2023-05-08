665ab7a484297cb16b508feb9d8791eb106518a7
Admin <exvita@yandex.ru>
2023-05-08 20:43:30 +0400

    Split branches

--- flask_stuff.py
+++ flask_stuff.py
@@ -6,110 +6,131 @@ from decorators import go_to_login
 from data import db_session
 
 
-from flask_login import LoginManager, login_user, logout_user, current_user, login_required
-#from flask_restful import abort
+from flask_login import (
+    LoginManager,
+    login_user,
+    logout_user,
+    current_user,
+    login_required,
+)
 
+# from flask_restful import abort
 
 
 @login_required
-@app.route('/')
-@go_to_login('/')
+@app.route("/")
+@go_to_login("/")
 def index():
-    return render_template('base.html', username = "EvgenyUtkin")
+    return render_template("base.html", username="EvgenyUtkin")
 
 
 @login_required
-@app.route('/feed')
-@go_to_login('/feed')
+@app.route("/feed")
+@go_to_login("/feed")
 def feed():
-    return render_template('feed.html')
+    return render_template("feed.html")
+
 
 @login_required
-@app.route('/contests')
-@go_to_login('/contests')
+@app.route("/contests")
+@go_to_login("/contests")
 def contests():
-    return render_template('contests.html')
+    return render_template("contests.html")
+
 
 @login_required
-@app.route('/collections')
-@go_to_login('/collections')
+@app.route("/collections")
+@go_to_login("/collections")
 def collections():
-    return render_template('collections.html')
+    return render_template("collections.html")
+
 
 @login_required
-@app.route('/editor')
-@go_to_login('/editor')
+@app.route("/editor")
+@go_to_login("/editor")
 def editor():
-    return render_template('editor.html')
+    return render_template("editor.html")
+
 
 @login_required
-@app.route('/profile/<username>')
-@go_to_login('/profile/<username>')
+@app.route("/profile/<username>")
+@go_to_login("/profile/<username>")
 def profile(username):
-    return render_template('profile.html', username = username)
+    return render_template("profile.html", username=username)
 
 
-@app.route('/register')
+@app.route("/register")
 def register():
-    return render_template('register.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
+    return render_template("register.html")
 
 
-@app.route('/login/<href>')
+@app.route("/login/<href>")
 def login(href):
-    return "Ooops!"
+    print(href)
+    return render_template("login.html", togo=f"/test_login/{href}")
 
 
-@app.route('/test_registration', methods=['POST', 'GET'])
+@app.route("/test_login/<href>", methods=["POST"])
+def test_login(href):
+    data = request.get_json()
+    login = data["login"]
+    password = data["password"]
+    print(login, password)
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).filter(User.name == login).first()
+    print(user)
+    if user and user.check_password(password):
+        login_user(user)
+        print("Login successful")
+        return href.replace("$", "/")
+    
+    return href.replace("$", "/")
+
+
+@app.route("/test_registration", methods=["POST", "GET"])
 def test_registration():
     data = request.get_json()
-    login = data['login']
-    email = data['email']
-    password = data['password']
-    repeat_password = data['repeat_password']
+    login = data["login"]
+    email = data["email"]
+    password = data["password"]
+    repeat_password = data["repeat_password"]
 
     db_sess = db_session.create_session()
     user = db_sess.query(User).filter(User.email == email).first()
     if user:
-        return "Такой пользователь уже есть"
-    
+        return "/register"
+
     if password != repeat_password:
-        return "Пароли не совпадают"
-    
+        return "/register"
+
     user = User(name=login, email=email)
 
     user.set_password(password)
 
-    login_user(user)
-
     db_sess.add(user)
     db_sess.commit()
+
+    login_user(user)
+
+    if not user.is_authenticated:
+        print("Registration failed")
+
+    print("Registration successful")
+    
     db_sess.close()
-
-
 
     # print(f"Login: {login}\nEmail: {email}\nPassword: {password}\nRepeat password: {repeat_password}")
 
     return "/feed"
 
-@app.route('/test_login', methods=['POST'])
-def test_login():
-    data = request.get_json()
-    login = data['login']
-    password = data['password']
-    print(f"Login: {login}\nPassword: {password}")
-    return "/feed"
 
 def main():
-
     db_session.global_init("database/blogs.db")
 
     port = int(os.environ.get("PORT", 5000))
-    #app.run(host='0.0.0.0', port=port)
-    app.run(port=8000, host='127.0.0.1')
+    # app.run(host='0.0.0.0', port=port)
+    app.run(port=8000, host="127.0.0.1")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
