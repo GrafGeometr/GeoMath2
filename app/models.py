68c2856f2fa1a0f8d3f4635dbaba695786bf448b
Ryan Gosling <xFunnyduck@yandex.ru>
2023-07-04 12:41:50 +0400

    Preparing DB for contests

--- app/models.py
+++ app/models.py
@@ -247,7 +247,7 @@ class Attachment(db.Model):
 
     short_name = db.Column(db.String)
 
-    parent_type = db.Column(db.String) # 'Problem' | 'Sheet' | 'Contest'
+    parent_type = db.Column(db.String) # 'Problem' | 'Sheet' | 'Contest_User_Solution'
     parent_id = db.Column(db.Integer)
 
     other_data = db.Column(db.JSON, default={})
@@ -257,6 +257,8 @@ class Attachment(db.Model):
             return Problem.query.filter_by(id = self.parent_id).first()
         elif self.parent_type == "Sheet":
             return Sheet.query.filter_by(id = self.parent_id).first()
+        elif self.parent_type == "Contest_User_Solution":
+            return Contest_User_Solution.query.filter_by(id = self.parent_id).first()
     
     def remove(self):
         try:
@@ -365,8 +367,34 @@ class Contest_User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     contest_id = db.Column(db.Integer, db.ForeignKey("contest.id"))
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    score = db.Column(db.JSON, default={})
+    contest_user_solutions = db.relationship("Contest_User_Solution", backref="contest_user")
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
     virtual = db.Column(db.Boolean, default=False)
-    virtual_start_date = db.Column(db.DateTime)
-    virtual_end_date = db.Column(db.DateTime)
-    other_data = db.Column(db.JSON, default={})
+
+    def is_started(self):
+        return self.start_date <= datetime.datetime.now()
+    
+    def is_ended(self):
+        return self.end_date <= datetime.datetime.now()
+    
+    def end_manually(self):
+        self.end_date = datetime.datetime.now()
+
+class Contest_User_Solution(db.Model):
+    __tablename__ = 'contest_user_solution'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    hashed_id = db.Column(db.String, unique=True)
+    contest_user_id = db.Column(db.Integer, db.ForeignKey("contest_user.id"))
+    problem_id = db.Column(db.Integer, db.ForeignKey("problem.id"))
+    score = db.Column(db.Integer)
+
+    def set_hashed_id(self):
+        while True:
+            hashed_id = generate_token(20)
+            if not Pool.query.filter_by(hashed_id = hashed_id).first():
+                self.hashed_id = hashed_id
+                break
+        
+        self.hashed_id = hashed_id
